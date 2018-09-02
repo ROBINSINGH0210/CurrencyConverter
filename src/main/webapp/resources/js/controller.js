da72dc0117616fcd2f005957a8edb169b40c39fc
Superman <Superman@DESKTOP-SUPERMAN>
2018-09-02 11:43:39 +0530

    updated the controller.

--- src/main/webapp/resources/js/controller.js
+++ src/main/webapp/resources/js/controller.js
@@ -1,8 +1,8 @@
 $.noConflict();
 jQuery(document).ready(function($) {
 	$("#datepicker").datepicker({
-		maxDate : -1,
-		minDate : new Date('2002-01-01'),
+		maxDate : 0,
+		minDate : new Date('2015-01-01'),
 		dateFormat : 'yy-mm-dd',
 		changeMonth : true,
 		changeYear : true
@@ -63,20 +63,20 @@ app.controller('getController', function($scope, $http, $location, $window) {
 
 	}
 
-	$scope.getCurrentRates = function() {
-
-		$http({
-			method : 'GET',
-			url : 'http://openexchangerates.org/api/latest.json?app_id=f1ef152621a3428e885d0ceb265fe7ba'
-		}).then(function successCallback(response) {
-			$scope.getDivAvailable = true;
-			$scope.currencyJson = response.data;
-			$scope.currencyJson.timestamp = response.data.timestamp * 1000;
-			$scope.saveData($scope.currencyJson);
-		}, function errorCallback(response) {
-			console.log(response);
-		});
-	}
+//	$scope.getCurrentRates = function() {
+//
+//		$http({
+//			method : 'GET',
+//			url : 'http://openexchangerates.org/api/latest.json?app_id=f1ef152621a3428e885d0ceb265fe7ba'
+//		}).then(function successCallback(response) {
+//			$scope.getDivAvailable = true;
+//			$scope.currencyJson = response.data;
+//			$scope.currencyJson.timestamp = response.data.timestamp * 1000;
+//			$scope.saveData($scope.currencyJson);
+//		}, function errorCallback(response) {
+//			console.log(response);
+//		});
+//	}
 
 	$scope.getHistoricRates = function() {
 		// 86400000 MiliSeconds in a day.
