35f5e8b6a00dc973603bf71e2915c152287e1876
Superman <Superman@DESKTOP-M4CL0EI>
2019-05-22 08:49:01 +0530

    Updated the project from Spring boot 1 to spring boot 2

--- src/main/java/com/currencyConvertor/main/WebApplication.java
+++ src/main/java/com/currencyConvertor/main/WebApplication.java
@@ -3,7 +3,7 @@ package com.currencyConvertor.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
